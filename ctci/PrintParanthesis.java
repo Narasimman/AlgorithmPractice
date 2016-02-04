9b8937c8b21071889a144acdd9163095d8dbdfa7
Narasimman <narasi.mit@gmail.com>
2016-02-04 14:45:24 -0500

    I don't know what

--- ctci/PrintParanthesis.java
+++ ctci/PrintParanthesis.java
@@ -2,18 +2,25 @@ package ctci;
 
 public class PrintParanthesis {
 
-  private static void printparan(int l, int r, char[] str, int count) {    
+  private static void printparan(int l, int r, char[] str, int count) {
+    //System.out.println(str);
+    //System.out.println(l + " " + r);
     if(l == 0 && r == 0) {
       System.out.println(str);
+      return;
     }
     
     if(l > 0) {
       str[count] = '(';
+      //System.out.println("Left: " + l);
+      //System.out.println("Count: " + count );
       printparan(l - 1, r, str, count + 1);
     }
     
     if(r > l) {
       str[count] = ')';
+      //System.out.println("Right: " + r);
+      //System.out.println("Count: " + count );
       printparan(l, r - 1, str, count + 1);
     }
   }
